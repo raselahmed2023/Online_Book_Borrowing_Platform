9f136ff437d4e9f4863fd3a8f13a3ff53a77d36b
Rasel Ahmed <raselahmediu22@gmail.com>
2026-04-30 12:32:31 +0600

    json data move from app to public folder

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
+import Card from "@/components/books/card";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <NavBar></NavBar>
+        <Card></Card>
         {children}
 
         <Footer></Footer>
