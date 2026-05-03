79f4388325569160a4382455fef528c884db03f2
Rasel Ahmed <raselahmediu22@gmail.com>
2026-05-03 16:09:46 +0600

    error er upor error

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-   env: {
+  serverExternalPackages: ["mongodb", "better-auth"],
+  env: {
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
   },
-  serverExternalPackages: ["mongodb"],
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'api.dicebear.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-      },
+      { protocol: 'https', hostname: 'api.dicebear.com' },
+      { protocol: 'https', hostname: 'i.ibb.co' },
     ],
   },
 };
-
 export default nextConfig;
