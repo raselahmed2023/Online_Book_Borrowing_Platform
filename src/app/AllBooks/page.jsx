450529c0b1357efa18cf604f6b4e644ae3789021
Rasel Ahmed <raselahmediu22@gmail.com>
2026-05-04 15:59:19 +0600

    all books content

--- src/app/AllBooks/page.jsx
+++ src/app/AllBooks/page.jsx
@@ -1,45 +1,10 @@
 "use client"
-import featureBook from "../../../public/books.json";
-import Image from 'next/image';
-import React from 'react';
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+import dynamic from "next/dynamic";
 
-const Page = () => {
-    const { data: session } = authClient.useSession();
-    const user = session?.user;
-    const router = useRouter();
+const AllBooks = dynamic(() => import("./AllBooksContent"), {
+  ssr: false,
+});
 
-    const handleViewDetails = (id) => {
-        if (!user) {
-            router.push("/Signin");
-        } else {
-            router.push(`/books/${id}`);
-        }
-    };
-
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 container mx-auto mt-5">
-            {featureBook.map(book => (
-                <div key={book.id} className="card bg-base-100 w-full shadow">
-                    <figure className="relative w-96 aspect-square">
-                        <Image src={book.image_url} alt={book.title} width={200} height={100}></Image>
-                    </figure>
-                    <div className="card-body items-center text-center space-y-4">
-                        <div className="badge badge-dash badge-primary absolute top-4 right-4">{book.category}</div>
-                        <h2 className="card-title m-0 text-2xl">{book.title}</h2>
-                        <h2 className="card-title m-0">{book.author}</h2>
-                        <button
-                            onClick={() => handleViewDetails(book.id)}
-                            className="btn btn-primary btn-sm"
-                        >
-                            View Details
-                        </button>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
-};
-
-export default Page;
+export default function AllBooksPage() {
+  return <AllBooks />;
+}
