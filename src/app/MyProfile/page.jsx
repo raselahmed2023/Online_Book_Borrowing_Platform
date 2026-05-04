2eecfb5d4971653c766a95e2bb129d0f018a0148
Rasel Ahmed <raselahmediu22@gmail.com>
2026-05-04 09:25:49 +0600

    checking and error

--- src/app/MyProfile/page.jsx
+++ src/app/MyProfile/page.jsx
@@ -1,14 +1,9 @@
+
+"use client"
 import dynamic from "next/dynamic";
 
 const MyProfile = dynamic(() => import("@/components/shared/MyProfile"), {
   ssr: false,
-  loading: () => {
-    return (
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
-        <span className="loading loading-spinner loading-lg"></span>
-      </div>
-    );
-  },
 });
 
 export default function MyProfilePage() {
--- src/app/MyProfile/update/page.jsx
+++ src/app/MyProfile/update/page.jsx
@@ -1,14 +1,8 @@
+"use client"
 import dynamic from "next/dynamic";
 
 const UpdateProfile = dynamic(() => import("@/components/shared/UpdateProfile"), {
   ssr: false,
-  loading: () => {
-    return (
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
-        <span className="loading loading-spinner loading-lg"></span>
-      </div>
-    );
-  },
 });
 
 export default function UpdateProfilePage() {
